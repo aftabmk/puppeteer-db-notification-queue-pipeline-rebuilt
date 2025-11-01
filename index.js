d62ddf99b123e307256a6c4f6f0a2a5bb5cc1e9e
Aftab mohammed k <87110394+aftabmk@users.noreply.github.com>
2025-11-01 17:04:37 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-const { DataStore } = require('./store');
-const { workflow, pageBuilder } = require('./utils');
-
+const { DataStore, WorkFlow } = require('./store')
+const { pageBuilder , buildBrowser} = require('./utils');
 
 const main = async () => {
-  console.time("start");
-  pageBuilder();
-  await workflow();
+  const manager = await buildBrowser();
+  
+  pageBuilder()
+  // const [pages1,pages2] = DataStore.getAllPages();
+  const pages = DataStore.getAllPages();
+  
+  const workflows = pages.map(page => new WorkFlow(manager,page));
 
-  const [ page1 ] = DataStore.getAllPages();
-  const payload = page1.getJsonData();
-  console.timeEnd("start");
-  debugger
-};
+  await Promise.allSettled(workflows.map(work => work.workflow()));
 
-main();
+  const arr = pages.map(page => page.getJsonData());
+
+
+  // await Promise.allSettled(workflows.map(work => work.workflowCache()));
+
+  // debugger;
+  manager.close();
+}
+
+const timer = async() => {
+  console.time("Browser")
+  await main()
+  console.timeEnd("Browser")
+}
+
+timer();
