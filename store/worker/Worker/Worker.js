8df8343289536c988a72138b08dd16d8b505a161
Aftab mohammed k <87110394+aftabmk@users.noreply.github.com>
2026-01-20 01:00:34 +0530

    modified:   store/worker/Worker/Worker.js

--- store/worker/Worker/Worker.js
+++ store/worker/Worker/Worker.js
@@ -41,7 +41,7 @@ class Worker extends WorkerUtils {
       });
 
       const response = await sns.send(command);
-      debugger;
+      // debugger;
       // set cache
       this.page.setCache();
 
